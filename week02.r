92dc653b772d35686497d98b0beda9aecb39f431
Krupa2016 <krupagurav16@gmail.com>
2026-08-06 14:37:05 +0530

    week 02

---
+++ week02.r
@@ -0,0 +1,48 @@
+# Krupa gurav
+# 24102C2005
+# BE CMPN C
+
+# Week 02: R as a Calculator with Scalars and Data Vectors : Addition, Subtraction, Multiplication & Division
+
+
+# 1. With Scalars
+x<- 10
+y<- 5
+
+# Addition
+add<- x+y
+print(add)
+
+# Subtraction
+sub<- x-y
+print(sub)
+
+# Multiplication
+mul<- x*y
+print(mul)
+
+# Division
+div<- x/y
+print(div)
+
+
+
+
+# 2. With Data Vectors
+my_vector <- c(2, 3, 5, 7)
+
+# Addition
+add_vector <- my_vector + 2
+print(add_vector)
+
+# Subtraction
+sub_vector <- my_vector - 1
+print(sub_vector)
+
+# Multiplication
+mul_vector <- my_vector * 3
+print(mul_vector)
+
+# Division
+div_vector <- my_vector / 2
+print(div_vector)
